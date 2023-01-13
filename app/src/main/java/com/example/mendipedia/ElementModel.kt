9b81f8d27e907a48e50f5cffef3b311d514c34bc
Olbabej <0lbabej@gmail.com>
2023-01-13 21:23:37 +0100

    remade the code

--- app/src/main/java/com/example/mendipedia/ElementModel.kt
+++ app/src/main/java/com/example/mendipedia/ElementModel.kt
@@ -1,8 +1,8 @@
 package com.example.mendipedia
 
-class ElementModel(name: String) {
-    public val name: String
-    init {
-        this.name = name
-    }
-}
+data class ElementModel(
+    var elementName: String,
+    var elementId: String,
+    var elementMass: String,
+    var elementDescription: String
+)
--- app/src/main/java/com/example/mendipedia/ElementsAdapter.kt
+++ app/src/main/java/com/example/mendipedia/ElementsAdapter.kt
@@ -1,27 +1,39 @@
 package com.example.mendipedia
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ElementsAdapter(val elementsList: List<ElementModel>) : RecyclerView.Adapter<ElementsAdapter.MyViewHolder>(){
-    class MyViewHolder(view:View) : RecyclerView.ViewHolder(view){
-        val name: TextView = view.findViewById(R.id.textViewElementName)
+class ElementsAdapter: RecyclerView.Adapter<ElementsAdapter.ViewHolder>() {
+
+    private var elementModelList: List<ElementModel> = arrayListOf()
+    private lateinit var context: Context
+
+    fun setData(elementModel: List<ElementModel>){
+        this.elementModelList = elementModel
+        notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.element_card, parent, false)
-        return  MyViewHolder(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.element_card, parent, false))
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val element = elementsList[position]
-        holder.name.text = element.name
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var elementModel = elementModelList.get(position)
+        var elementName = elementModel.elementName
+
+        holder.textViewElementName.text = elementName
     }
 
     override fun getItemCount(): Int {
-        return elementsList.size
+        return elementModelList.size
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var textViewElementName = itemView.findViewById<TextView>(R.id.textViewElementName)
     }
 }
